02d2527e5ebd36ea5cf532fdb605b2f0183402ae
chsu <yaostyle@gmail.com>
2018-07-24 17:58:34 -0700

    First commit

---
+++ lib/pages/auth.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthPageState();
+  }
+}
+
+class _AuthPageState extends State<AuthPage> {
+  String _emailValue;
+  String _passwordValue;
+  bool _acceptTerms = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(10.0),
+          child: ListView(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: "E-mail"),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (String value) {
+                  setState(() {
+                    _emailValue = value;
+                  });
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Password"),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    _passwordValue = value;
+                  });
+                },
+              ),
+              SwitchListTile(
+                value: _acceptTerms,
+                onChanged: (bool value) {
+                  setState(() {
+                                      _acceptTerms = value;
+                                    });
+                },
+                title: Text('Accept Terms'),
+              ),
+              SizedBox(height: 10.0),
+              RaisedButton(
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+                child: Text('LOGIN'),
+                onPressed: () {
+                  print(_emailValue);
+                  print(_passwordValue);
+                  Navigator.pushReplacementNamed(context, '/products');
+                },
+              ),
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/pages/products.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../product_manager.dart';
+
+class ProductsPage extends StatelessWidget {
+  final List<Map<String, dynamic>> products;
+
+  ProductsPage(this.products);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage Products'),
+              onTap: () {
+                Navigator.pushReplacementNamed(context, '/admin');
+              },
+            )
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text('EasyList'),
+      ),
+      body: ProductManager(products),
+    );
+  }
+}
