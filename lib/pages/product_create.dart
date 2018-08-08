3996b5d3f7b59dc314cfc3f2916131dff58a75f2
chsu <yaostyle@gmail.com>
2018-08-08 11:59:06 -0700

    Use Form widgets and TextFormFields to store form state

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -15,85 +15,89 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   String _titleValue;
   String _descriptionValue;
   double _priceValue;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _buildTitleTextField() {
-    return TextField(
-            decoration: InputDecoration(labelText: 'Product Title'),
-            onChanged: (String value) {
-              setState(() {
-                _titleValue = value;
-              });
-            },
-          );
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Product Title'),
+      onSaved: (String value) {
+        setState(() {
+          _titleValue = value;
+        });
+      },
+    );
   }
 
   Widget _buildDescriptionTextField() {
-    return TextField(
-            maxLines: 4,
-            decoration: InputDecoration(labelText: 'Product Description'),
-            onChanged: (String value) {
-              setState(() {
-                _descriptionValue = value;
-              });
-            },
-          );
+    return TextFormField(
+      maxLines: 4,
+      decoration: InputDecoration(labelText: 'Product Description'),
+      onSaved: (String value) {
+        setState(() {
+          _descriptionValue = value;
+        });
+      },
+    );
   }
 
   void _submitForm() {
-              final Map<String, dynamic> product = {
-                'title': _titleValue,
-                'description': _descriptionValue,
-                'price': _priceValue,
-                'image': 'assets/food.jpg'
-              };
-              widget.addProduct(product);
-              Navigator.pushReplacementNamed(context, '/products');    
+    _formKey.currentState.save();
+    final Map<String, dynamic> product = {
+      'title': _titleValue,
+      'description': _descriptionValue,
+      'price': _priceValue,
+      'image': 'assets/food.jpg'
+    };
+    widget.addProduct(product);
+    Navigator.pushReplacementNamed(context, '/products');
   }
 
   Widget _buildPriceTextField() {
-    return           TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(labelText: 'Product Price'),
-            onChanged: (String value) {
-              setState(() {
-                _priceValue = double.parse(value);
-              });
-            },
-          );
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(labelText: 'Product Price'),
+      onSaved: (String value) {
+        setState(() {
+          _priceValue = double.parse(value);
+        });
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-
     final double deviceWidth = MediaQuery.of(context).size.width;
     final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
     final double targetPadding = deviceWidth - targetWidth;
 
     return Container(
       margin: EdgeInsets.all(10.0),
-      child: ListView(
-        padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
-        children: <Widget>[
-          _buildTitleTextField(),
-          _buildDescriptionTextField(),
-          _buildPriceTextField(),
-          SizedBox(
-            height: 10.0,
-          ),
-          RaisedButton(
-            child: Text('Save'),
-            textColor: Colors.white,
-            onPressed: _submitForm,
-          )
-          // GestureDetector(
-          //   onTap: _submitForm,
-          //   child: Container(
-          //     color: Colors.green,
-          //     padding: EdgeInsets.all(5.0),
-          //     child: Text('My Button'),
-          //   ),
-          // )
-        ],
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
+          children: <Widget>[
+            _buildTitleTextField(),
+            _buildDescriptionTextField(),
+            _buildPriceTextField(),
+            SizedBox(
+              height: 10.0,
+            ),
+            RaisedButton(
+              child: Text('Save'),
+              textColor: Colors.white,
+              onPressed: _submitForm,
+            )
+            // GestureDetector(
+            //   onTap: _submitForm,
+            //   child: Container(
+            //     color: Colors.green,
+            //     padding: EdgeInsets.all(5.0),
+            //     child: Text('My Button'),
+            //   ),
+            // )
+          ],
+        ),
       ),
     );
   }
