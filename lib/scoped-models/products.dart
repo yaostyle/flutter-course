f8ca6f7a13042f5bf7ff623dc2ced2fe5b22e8ce
chsu <yaostyle@gmail.com>
2018-08-15 15:34:50 -0700

    Update model & scoped model for toggling Favorite status

--- lib/scoped-models/products.dart
+++ lib/scoped-models/products.dart
@@ -36,6 +36,19 @@ class ProductsModel extends Model {
     _selectedProductIndex = null;
   }
 
+  void toggleProductFavoriteStatus() {
+    final bool isCurrentlyFavorite = selectedProduct.isFavorite;
+    final bool newFavoriteStatus = !isCurrentlyFavorite;
+    final Product updateProduct = Product(
+        title: selectedProduct.title,
+        description: selectedProduct.description,
+        price: selectedProduct.price,
+        image: selectedProduct.image,
+        isFavorite: newFavoriteStatus);
+      _products[_selectedProductIndex] = updateProduct;
+    _selectedProductIndex = null;
+  }
+
   void selectProduct(int index) {
     _selectedProductIndex = index;
   }
